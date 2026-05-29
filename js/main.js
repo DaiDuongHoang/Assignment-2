0a4df6ffe9750ce316f2f20cdeae61d765d137a1
Duong <dhoa0014@student.monash.edu>
2026-05-30 04:05:39 +1000

    checkpoint

--- js/main.js
+++ js/main.js
@@ -3,9 +3,9 @@ const stateBivariateBurnMapSpec = "js/state_bivariate_burn_map.vg.json?v=2026052
 const streamgraphSpec = "js/streamgraph.vg.json?v=20260526_v13";
 const bushfireTimelineSpec = "js/bushfire_event_timeline.vg.json?v=20260529_full_width_v21";
 
-const economicShockSpec = "js/economic_shock.vg.json?v=20260529_bg_contrast_v1";
+const economicShockSpec = "js/economic_shock.vg.json?v=20260530_compact_v1";
 const domesticCommercialClaimsSpec = "js/domestic_commercial_claims.vg.json?v=20260529_legend_hover_v1";
-const threatenedAnimalsByClassSpec = "js/threatened_animals_by_class.vg.json?v=20260530_fig4b_insight_v1";
+const threatenedAnimalsByClassSpec = "js/threatened_animals_by_class.vg.json?v=20260530_fig4b_editorial_v2";
 
 // Embed the Fire.csv hotspot map (Row 1)
 vegaEmbed("#vis-fire-hotspots", fireHotspotsSpec, { "actions": false })
