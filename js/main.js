c1413fd213404cfef6e5445efb7419de009e5b5a
Duong <dhoa0014@student.monash.edu>
2026-05-31 01:46:51 +1000

    feat: implement Vega-Lite visualizations with custom interactivity in main.js and update index.html structure

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@ const fireHotspotsSpec = "js/map_fire_hotspots.vg.json?v=20260530_control_spacin
 const stateBivariateBurnMapSpec = "js/state_bivariate_burn_map.vg.json?v=20260529_clean_tooltip_v22";
 const fireRiskTrajectoriesSpec = "js/fire_risk_trajectories.vg.json?v=20260530_state_profiles_v14";
 const streamgraphSpec = "js/streamgraph.vg.json?v=20260530_v15";
-const annualBurnedAreaExtremesSpec = "js/annual_burned_area_extremes.vg.json?v=20260530_shift_v8";
+const annualBurnedAreaExtremesSpec = "js/annual_burned_area_extremes.vg.json?v=20260530_shift_v12";
 const bushfireTimelineSpec = "js/bushfire_event_timeline.vg.json?v=20260529_full_width_v21";
 
 const economicShockSpec = "js/economic_shock.vg.json?v=20260530_compact_v1";
