1f71f530b8afac05f551f702fd01f3f62c0467b9
Duong <dhoa0014@student.monash.edu>
2026-05-31 23:29:22 +1000

    feat: add bubble scatter plot for burned area versus economic loss and update index page metadata and footer

--- js/main.js
+++ js/main.js
@@ -9,7 +9,7 @@ const bushfireTimelineSpec = "js/bushfire_event_timeline.vg.json?v=20260531_prem
 
 const economicShockSpec = "js/economic_shock.vg.json?v=20260531_zoompan_scales_v9";
 const domesticCommercialClaimsSpec = "js/domestic_commercial_claims.vg.json?v=20260529_legend_hover_v3";
-const burnedAreaLossSpec = "js/burned_area_vs_economic_loss.vg.json?v=20260531_fig3d_restore_v5";
+const burnedAreaLossSpec = "js/burned_area_vs_economic_loss.vg.json?v=20260531_bubble_polish_v12";
 const threatenedAnimalsByClassSpec = "js/threatened_animals_by_class.vg.json?v=20260531_fig4b_editorial_v4";
 
 // Embed the Fire.csv hotspot map (Row 1)
